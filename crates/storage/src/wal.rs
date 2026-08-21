f112d8a3fc710758c38a046306168e281d8b90b3
ilijavlahovic24-bit <ilija.vlahovic24@gmail.com>
2026-08-21 13:27:03 +0200

    added stub for DataPath

--- crates/storage/src/wal.rs
+++ crates/storage/src/wal.rs
@@ -4,7 +4,7 @@ use std::sync::atomic::AtomicU64;
 use tokio::fs::File;
 pub use common::types::InodeId;
 use common::error::FfsError;
-enum Waloperation{
+pub enum WalOperation {
     Create { inode_id: InodeId, parent_id: InodeId, name: String },
     Unlink { parent_id: InodeId, name: String },
     Mkdir  { inode_id: InodeId, parent_id: InodeId, name: String },
@@ -15,27 +15,35 @@ enum Waloperation{
 struct DataPath{
     tmp_rid:PathBuf,
     data_path:PathBuf,
-    wal:Arc<MetadataWal>
+    wal:Arc<MetadataWall>
+}
+impl DataPath{
+    fn write_blob(inode_id: InodeId, data: &[u8]) -> Result<(), FfsError>{
+        todo!()
+    }
+    fn read_blob(inode_id: InodeId) -> Result<Vec<u8>, FfsError>{
+        todo!()
+    }
 }
 
 struct WalEntry{
     sequence: u64,
-    operation: Waloperation,
+    operation: WalOperation,
     checksum: u32,
 }
 
 #[allow(unused)]
-struct MetadataWal{
+struct MetadataWall {
     path:PathBuf,
     file:File,
     sequence:AtomicU64
 }
 #[allow(unused)]
-impl MetadataWal {
+impl MetadataWall {
     pub fn new(path: PathBuf) -> Result<Self, FfsError>{
         todo!()
     }
-    pub fn append(&mut self, op: Waloperation) -> Result<(), FfsError>{
+    pub fn append(&mut self, op: WalOperation) -> Result<(), FfsError>{
         todo!()
     }
     pub fn read_all(path: &Path) -> Result<Vec<WalEntry>, FfsError>{
